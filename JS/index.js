f87d573bac5ddedae742e9f0610a0b7f96d09a13
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-01 11:23:56 -0500

    Add js

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+console.log("¡Hola mundo!");
+
+var x, x;
+x = [];
+
+var matriz = [
+  ["R", 0, 1.2, x, "A"],
+  [true, "O", 3, "D", 4.5],
+  [x, false, "N", 6, 7.8],
+  [x, "O", true, "C", 9],
+  ["R", 11.2, x, false, "O"],
+];
+
+console.log(
+  matriz[0][0],
+  matriz[1][1],
+  matriz[2][2],
+  matriz[3][3],
+  matriz[4][4]
+);
+
+console.log(
+  matriz[4][0],
+  matriz[3][1],
+  matriz[2][2],
+  matriz[1][3],
+  matriz[0][4]
+);
