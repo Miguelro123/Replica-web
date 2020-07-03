64961f837d4ed9bf0e7543ba5df3daea26aefac9
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-02 23:33:36 -0500

    Add js

---
+++ JS/string_indexOF.js
@@ -0,0 +1,27 @@
+var one = "es una prueba";
+var two = "es otra prueba";
+var three = "prueba";
+
+if (one.indexOf(two) != -1 {
+  console.log("'" + two + "'está contenido dentro de'" + one + "'");
+}
+
+if (one.indexOf(three) != -1 {
+  console.log("'" + three + "'está contenido dentro de'" + one + "'");
+}
+
+if (two.indexOf(one) != -1 {
+  console.log("'" + one + "'está contenido dentro de'" + two + "'");
+}
+
+if (two.indexOf(three) != -1 {
+  console.log("'" + three + "'está contenido dentro de'" + two + "'");
+}
+
+if (three.indexOf(one) != -1 {
+  console.log("'" + one + "'está contenido dentro de'" + three + "'");
+}
+
+if (three.indexOf(two) != -1 {
+  console.log("'" + two + "'está contenido dentro de'" + three + "'");
+}
