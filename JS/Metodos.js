6f2df8778758ac3b61ff072858630b6cc15f3e9f
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-07 08:52:35 -0500

    Add js

---
+++ JS/Metodos.js
@@ -0,0 +1,58 @@
+var cadena = "Como abajo arriba";
+var otraCadena = "Todos es Uno";
+
+// CHARAT
+
+cadena.charAt(2)
+
+cadena.charCodeAt(2)
+
+// CONCAT
+
+cadena.concat(otraCadena)
+
+// INDEXOF
+
+otraCadena.indexOf('e')
+
+// LASTINDEXOF
+
+cadena.lastIndexOf('o')
+
+// MATCH
+
+cadena.match('abajo')
+
+// REPLACE
+
+cadena.replace('abajo','dentro')
+
+// SEARCH
+
+cadena.search('abajo')
+
+// SLICE
+
+otraCadena.slice(3,7)
+
+// SPLIT
+
+otraCadena.split('os')
+
+
+
+var colores = ["rojo", "azul ", " verde "] ; 
+
+// length
+
+colores.length; 
+
+colores[colores.length] = "amarillo";
+
+// toString
+
+colores.toString();}
+
+colores.push ( "negro", "blanco"); 
+
+colores.pop(); 
