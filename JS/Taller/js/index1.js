cd561b079d0b11ed8845c955e156d59233af3b42
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-21 22:56:01 -0500

    Add js

---
+++ JS/Taller/js/index1.js
@@ -0,0 +1,41 @@
+// http://www.lawebdelprogramador.com
+
+/**
+ * Funcion que añade un <li> dentro del <ul>
+ */
+function add_li() {
+  var nuevoLi = document.getElementById("nuevo_li").value;
+  if (nuevoLi.length > 0) {
+    if (find_li(nuevoLi)) {
+      var li = document.createElement("li");
+      li.id = nuevoLi;
+      li.innerHTML = "<span onclick='eliminar(this)'>X</span>" + nuevoLi;
+      document.getElementById("listaDesordenada").appendChild(li);
+    }
+  }
+  return false;
+}
+
+/**
+ * Funcion que busca si existe ya el <li> dentrol del <ul>
+ * Devuelve true si no existe.
+ */
+function find_li(contenido) {
+  var el = document
+    .getElementById("listaDesordenada")
+    .getElementsByTagName("li");
+  for (var i = 0; i < el.length; i++) {
+    if (el[i].innerHTML == contenido) return false;
+  }
+  return true;
+}
+
+/**
+ * Funcion para eliminar los elementos
+ * Tiene que recibir el elemento pulsado
+ */
+function eliminar(elemento) {
+  var id = elemento.parentNode.getAttribute("id");
+  node = document.getElementById(id);
+  node.parentNode.removeChild(node);
+}
