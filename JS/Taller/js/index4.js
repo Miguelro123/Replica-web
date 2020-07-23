33937947543f599b2ebd36820caff334ceb68073
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-22 23:49:23 -0500

    Update js

--- JS/Taller/js/index4.js
+++ JS/Taller/js/index4.js
@@ -1,1 +1,21 @@
-js
+function formu() {
+  var nombre = document.getElementById("name").value;
+  var correo = document.getElementById("email").value;
+  var direccion = document.getElementById("direccion").value;
+  var edad = document.getElementById("edad").value;
+  var ocupacion = document.getElementById("profesion").value;
+  var telefono = document.getElementById("tel").value;
+
+  var usuario = {
+    name: nombre,
+    mail: correo,
+    direccion: direccion,
+    age: edad,
+    occupation: ocupacion,
+    tel: telefono,
+  };
+
+  console.log(usuario);
+
+  return false;
+}
